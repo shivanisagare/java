c3674a64fc9585d9a0632e3632de878930539b45
shivanisagare <167905023+shivanisagare@users.noreply.github.com>
2024-10-04 21:53:51 +0530

    Create Basicoperation.java

---
+++ Basicoperation.java
@@ -0,0 +1,28 @@
+import java.io.*;
+class BasicOperation
+{
+public static void main(String args[])
+{
+
+int a=30;
+int b=10;
+int c;
+System.out.println("Enter the value of a :" + a);
+System.out.println("Enter the value of b :" + b);
+c=a+b;
+System.out.println("Addition is " + c);
+
+c=a-b;
+System.out.println("Substraction is " + c);
+
+c=a*b;
+System.out.println("Multiplication is " + c);
+
+c=a/b;
+System.out.println("Division is " + c);
+
+c=a%b;
+System.out.println("Modulus is " + c);
+}
+
+}
